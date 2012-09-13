4cf95dc648f755e2aade8744eea1ea65e009c706
kempkes <kempkes@gmail.com>
2012-09-12 19:09:15 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Thomas J. Kempkes"
+    email    "kempkes@gmail.com"
+    password "tarlY666"
+    password_confirmation "tarlY666"
+  end
+end
